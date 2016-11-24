96669c13700d71964e034208f20a755d8e70cb29
Patrick Rainsberry <patrick.rainsberry@gmail.com>
2016-11-24 13:51:19 -0800

    Added user parameter save

---
+++ paramSaverCommand.py
@@ -0,0 +1,178 @@
+import adsk.core, adsk.fusion, traceback
+
+from xml.etree import ElementTree
+from xml.etree.ElementTree import SubElement
+
+from .Fusion360CommandBase import Fusion360NavCommandBase
+
+groupName = 'tapnair-stateSaver'
+attribName = 'paramSaver'
+
+def write_XML_paramState(root, newState):
+    
+    app = adsk.core.Application.get()
+    design = adsk.fusion.Design.cast(app.activeProduct)
+    
+    # Create a new State in the xml tree
+    state = SubElement( root, 'state', name=newState)
+
+    userParams = design.userParameters
+    for param in userParams:
+   
+        # Record parameter value state
+        if param is not None:               
+            SubElement( state, 'parameter', value=str(param.value), name=param.name)    
+    
+    xmlstr = ElementTree.tostring(root, encoding='unicode', method='xml')
+    
+    design.attributes.add(groupName, attribName, xmlstr)
+
+def read_XML_paramState(root, state):
+    app = adsk.core.Application.get()
+    design = adsk.fusion.Design.cast(app.activeProduct)
+
+    # Get All parameters in design
+    allParams = design.allParameters
+    for param in allParams:
+
+        # Apply Saved dimension info
+        if param is not None:                       
+            test = root.find("state[@name='%s']/parameter[@name='%s']" % (state, param.name))
+            if test is not None:
+                param.value = float(test.get('value'))
+                
+def getXML_from_Attribute(groupName, attributeName):
+    app = adsk.core.Application.get()
+    design_ = adsk.fusion.Design.cast(app.activeProduct)
+
+    attrib = design_.attributes.itemByName(groupName, attributeName)
+    
+    # Get XML Root node
+    if attrib is not None:
+        root = ElementTree.fromstring(attrib.value)
+        
+    else:
+        root = ElementTree.Element('paramSaves')
+
+    return root
+
+def updateParams(inputs):
+    
+    # Get Fusion Objects
+    app = adsk.core.Application.get()
+    ui  = app.userInterface
+    design = app.activeProduct
+    unitsMgr = design.unitsManager
+    
+    if inputs.count < 1:
+        ui.messageBox('No User Parameters in the model')
+        return          
+    
+    # Set all parameter values based on the input form                            
+    for param in design.userParameters:
+        inputExpresion = inputs.itemById(param.name).value
+        
+        # Use Fusion Units Manager to validate user expresion                        
+        if unitsMgr.isValidExpression(inputExpresion, unitsMgr.defaultLengthUnits):
+            
+            # Set parameter value from input form                         
+            param.expression = inputExpresion
+        
+        else:
+            ui.messageBox("The following expresion was invalid: \n" +
+                            param.name + '\n' +
+                            inputExpresion)
+
+############# Create your Actions Here #################################################
+class paramSaveCommand(Fusion360NavCommandBase):
+    
+    # Runs when Fusion command would generate a preview after all inputs are valid or changed
+    def onPreview(self, command, inputs):
+        pass
+    
+    # Runs when the command is destroyed.  Sometimes useful for cleanup after the fact
+    def onDestroy(self, command, inputs, reason_):    
+        pass
+    
+    # Runs when when any input in the command dialog is changed
+    def onInputChanged(self, command, inputs, changedInput):
+        pass
+    
+    # Runs when the user presses ok button
+    def onExecute(self, command, inputs):
+        root = getXML_from_Attribute(groupName, attribName)
+        write_XML_paramState(root, inputs.itemById('newName').value)
+    
+    # Runs when user selects your command from Fusion UI, Build UI here
+    def onCreate(self, command, inputs):
+        
+        # Create a few inputs in the UI
+        inputs.addStringValueInput('newName', 'New Config Name:', 'New Config')  
+        
+############# Create your Actions Here #################################################
+class paramSwitchCommand(Fusion360NavCommandBase):
+    
+    # Runs when Fusion command would generate a preview after all inputs are valid or changed
+    def onPreview(self, command, inputs):
+        state = inputs.itemById('currentState').selectedItem.name
+        if state != 'Current':
+            root = getXML_from_Attribute(groupName, attribName)
+            if root:
+                read_XML_paramState(root, state)
+    
+    # Runs when the command is destroyed.  Sometimes useful for cleanup after the fact
+    def onDestroy(self, command, inputs, reason_):    
+        pass
+    
+    # Runs when when any input in the command dialog is changed
+    def onInputChanged(self, command, inputs, changedInput):
+        pass
+    
+    # Runs when the user presses ok button
+    def onExecute(self, command, inputs):
+
+        state = inputs.itemById('currentState').selectedItem.name
+        root = getXML_from_Attribute(groupName, attribName)
+        if root:
+            read_XML_paramState(root, state)
+
+    # Runs when user selects your command from Fusion UI, Build UI here
+    def onCreate(self, command, inputs):
+        
+        dropDown = inputs.addDropDownCommandInput('currentState', 'Select Saved Config:', adsk.core.DropDownStyles.TextListDropDownStyle)
+        dropDownItems = dropDown.listItems
+        dropDownItems.add('Current', True)
+        
+        root = getXML_from_Attribute(groupName, attribName)
+        if root:
+            for state in root.findall('state'):
+                dropDownItems.add(state.get('name'), False,)
+
+############# Create your Actions Here #################################################
+class paramEditCommand(Fusion360NavCommandBase):
+    
+    # Runs when Fusion command would generate a preview after all inputs are valid or changed
+    def onPreview(self, command, inputs):
+        updateParams(inputs)
+    
+    # Runs when the command is destroyed.  Sometimes useful for cleanup after the fact
+    def onDestroy(self, command, inputs, reason_):    
+        pass
+    
+    # Runs when when any input in the command dialog is changed
+    def onInputChanged(self, command, inputs, changedInput):
+        pass
+    
+    # Runs when the user presses ok button
+    def onExecute(self, command, inputs):
+        updateParams(inputs)
+    
+    # Runs when user selects your command from Fusion UI, Build UI here
+    def onCreate(self, command, inputs):              
+        app = adsk.core.Application.get()
+        design = app.activeProduct
+        for param in design.userParameters:                                         
+            #if param.name[0] != '_':
+            inputs.addStringValueInput(param.name,
+                                       param.name,
+                                       param.expression)
--- stateSaver.py
+++ stateSaver.py
@@ -2,36 +2,91 @@
 # Could import multiple Command definitions here
 from .configSaverCommand import configSaveCommand, configSwitchCommand, unsuppressAllCommand
 from .displaySaverCommand import displaySaveCommand, displaySwitchCommand
+from .paramSaverCommand import paramSaveCommand, paramSwitchCommand, paramEditCommand
 
-#### Define parameters for 2nd command #####
-commandName1 = 'Save Configuration'
-commandDescription1 = 'Saves the current State of suppression in the model'
-commandResources1 = './resources/CS'
-cmdId1 = 'CS_CmdId'
+commands = []
+command_defs =[]
 
-#### Define parameters for 2nd command #####
-commandName2 = 'Switch Configuration'
-commandDescription2 = 'Allows you to switch to existing Configurations in the Model'
-commandResources2 = './resources/CS'
-cmdId2 = 'SWC_CmdId'
+#### Define parameters for command #####
+cmd = {
+        'commandName' : 'Save Configuration',
+        'commandDescription' : 'Saves the current State of suppression in the model',
+        'commandResources' : './resources/CS',
+        'cmdId' : 'CS_CmdId',
+        'class' : configSaveCommand
+}
+command_defs.append(cmd)
 
-#### Define parameters for 3rd command #####
-commandName3 = 'Save Display'
-commandDescription3 = 'Save Current Display State Of Components'
-commandResources3 = './resources/displaySave'
-cmdId3 = 'DisplaySave_CmdId'
+#### Define parameters for command #####
+cmd = {
+        'commandName' : 'Switch Configuration',
+        'commandDescription' : 'Allows you to switch to existing Configurations in the Model',
+        'commandResources' : './resources/CS',
+        'cmdId' : 'SWC_CmdId',
+        'class' : configSwitchCommand
+}
+command_defs.append(cmd)
 
-#### Define parameters for 3rd command #####
-commandName4 = 'Switch Display'
-commandDescription4 = 'Switch Display State Of Components'
-commandResources4 = './resources/displaySave'
-cmdId4 = 'DisplaySwitch_CmdId'
+#### Define parameters for command #####
+cmd = {
+        'commandName' : 'Save Display',
+        'commandDescription' : 'Save Current Display State Of Components',
+        'commandResources' : './resources/displaySave',
+        'cmdId' : 'DisplaySave_CmdId',
+        'class' : displaySaveCommand
+}
+command_defs.append(cmd)
 
-#### Define parameters for 3rd command #####
-commandName5 = 'Unsuppress All'
-commandDescription5 = 'Unsuppresses all features in the timeline'
-commandResources5 = './resources/CS'
-cmdId5 = 'unsuppressAll_CmdId'
+#### Define parameters for command #####
+cmd = {
+        'commandName' : 'Switch Display',
+        'commandDescription' : 'Switch Display State Of Components',
+        'commandResources' : './resources/displaySave',
+        'cmdId' : 'DisplaySwitch_CmdId',
+        'class' : displaySwitchCommand
+}
+command_defs.append(cmd)
+
+#### Define parameters for command #####
+cmd = {
+        'commandName' : 'Unsuppress All',
+        'commandDescription' : 'Unsuppresses all features in the timeline',
+        'commandResources' : './resources/CS',
+        'cmdId' : 'unsuppressAll_CmdId',
+        'class' : unsuppressAllCommand
+}
+command_defs.append(cmd)
+
+#### Define parameters for command #####
+cmd = {
+        'commandName' : 'Edit Parameters',
+        'commandDescription' : 'Quickly Edit User Parameters',
+        'commandResources' : './resources/CS',
+        'cmdId' : 'EP_CmdId',
+        'class' : paramEditCommand
+}
+command_defs.append(cmd)
+
+#### Define parameters for command #####
+cmd = {
+        'commandName' : 'Save Parameters',
+        'commandDescription' : 'Save the Values of all User Parameters',
+        'commandResources' : './resources/CS',
+        'cmdId' : 'SP_CmdId',
+        'class' : paramSaveCommand
+}
+command_defs.append(cmd)
+
+#### Define parameters for command #####
+cmd = {
+        'commandName' : 'Switch Parameters',
+        'commandDescription' : 'Switch between saved sets of user Parameters',
+        'commandResources' : './resources/CS',
+        'cmdId' : 'SWP_CmdId',
+        'class' : paramSwitchCommand
+}
+command_defs.append(cmd)
+
 
 #### Define parameters for Drop Down Command #####
 DC_Resources = './resources/DC'
@@ -40,24 +95,18 @@ DC_CmdId = 'stateSaver'
 # Set to True to display various useful messages when debugging your app
 debug = False
 
-# Creates the commands for use in the Fusion 360 UI
-newCommand1 = configSaveCommand(commandName1, commandDescription1, commandResources1, cmdId1, DC_CmdId, DC_Resources, debug)
-newCommand2 = configSwitchCommand(commandName2, commandDescription2, commandResources2, cmdId2, DC_CmdId, DC_Resources, debug)
-newCommand3 = displaySaveCommand(commandName3, commandDescription3, commandResources3, cmdId3, DC_CmdId, DC_Resources, debug)
-newCommand4 = displaySwitchCommand(commandName4, commandDescription4, commandResources4, cmdId4, DC_CmdId, DC_Resources, debug)
-newCommand5 = unsuppressAllCommand(commandName5, commandDescription5, commandResources5, cmdId5, DC_CmdId, DC_Resources, debug)
+for cmd_def in command_defs:
+    # Creates the commands for use in the Fusion 360 UI
+    command = cmd_def['class'](cmd_def['commandName'], cmd_def['commandDescription'], cmd_def['commandResources'], cmd_def['cmdId'], DC_CmdId, DC_Resources, debug)
+    commands.append(command)
 
 def run(context):
-    newCommand1.onRun()
-    newCommand2.onRun()
-    newCommand3.onRun()
-    newCommand4.onRun()
-    newCommand5.onRun()
+    for command in commands:
+        command.onRun()
+
 
 def stop(context):
-    newCommand1.onStop()
-    newCommand2.onStop()
-    newCommand3.onStop()
-    newCommand4.onStop()
-    newCommand5.onStop()
+    for command in commands:
+        command.onStop()
+
 
